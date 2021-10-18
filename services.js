12a629d7aff4dffd3b3c0eced2ba2206667c6e30
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-10-18 14:25:17 +0600

    added service image and info

---
+++ services.js
@@ -0,0 +1,65 @@
+[
+  {
+    id: "service-001",
+    title: "Gynecology",
+    description:
+      "When asked the question, responses will be superficial at best, and at worst.",
+    img: "",
+  },
+  {
+    id: "service-002",
+    title: "Orthopaedics",
+    description:
+      "The sad thing is the majority of people have no clue about what they truly want.",
+    img: "",
+  },
+  {
+    id: "service-003",
+    title: "Neurology",
+    description:
+      "Trying to go through life without clarity is similar to sailing",
+    img: "",
+  },
+  {
+    id: "service-004",
+    title: "Eye Care",
+    description:
+      "You are going on a cruise, but when the ship sets sail, you discover it has no rudder.",
+    img: "",
+  },
+  {
+    id: "service-005",
+    title: "Medicine",
+    description:
+      "Reflect and experiment until you find the right combination of motivators",
+    img: "",
+  },
+  {
+    id: "service-006",
+    title: "Dental Care",
+    description:
+      "Use a past defeat as a motivator. Remind yourself you have nowhere",
+    img: "",
+  },
+  {
+    id: "service-007",
+    title: "Pediatric",
+    description:
+      "It must come from within as the natural product of your desire to achieve something.",
+    img: "",
+  },
+  {
+    id: "service-008",
+    title: "Urology",
+    description:
+      "Something that someone else can give you you are the only one with the power to motivate you.",
+    img: "",
+  },
+  {
+    id: "service-009",
+    title: "Cardiology",
+    description:
+      "I truly believe Augustine’s words are true and if you look at history you know it is true.",
+    img: "",
+  },
+];
